7a5bfe47e468766f759c9748cb7b4ac60a2110ab
NuoWenLei <nuowen0612@gmail.com>
2024-08-03 11:52:14 -0700

    add basic openai stuff

---
+++ app.py
@@ -0,0 +1,43 @@
+import os, random
+from dotenv import load_dotenv
+from typing import Union
+from fastapi import FastAPI
+from openai import OpenAI
+
+from constants import characterPrompt, systemPrompt, mapping, reverseMapping, maximumMovements
+
+load_dotenv()
+
+client = OpenAI(
+  # This is the default and can be omitted
+  api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+app = FastAPI()
+
+@app.get("/generateCharacterActions")
+def generateActions(description: str):
+
+  prompt = f"""{characterPrompt}{description}
+
+List of actions:
+"""
+  
+  print(prompt)
+
+  completion = client.chat.completions.create(
+    messages=[
+        {"role": "system", "content": systemPrompt},
+        {"role": "user", "content": prompt}
+    ],
+    model="gpt-4o-mini",
+  )
+
+  msg = completion.choices[0].message
+  print(msg)
+  mappedActions = mapping.values()
+  filteredCharacters = [reverseMapping[c] for c in msg.content if c in mappedActions]
+  return filteredCharacters[:maximumMovements]
+    
+    
+
---
+++ constants.py
@@ -0,0 +1,39 @@
+maximumMovements = 10
+
+mapping = {
+	"Up": "U",
+	"Down": "D",
+	"Left": "L",
+	"Right": "R",
+	"Wait": "W",
+	"Attack": "A",
+	"Swing": "A",
+	"Blast": "A",
+	"Punch": "A"
+}
+
+reverseMapping = {
+		"U": "Up",
+	"D": "Down",
+	"L": "Left",
+	"R": "Right",
+	"W": "Wait",
+	"A": "Attack",
+}
+
+formattedMapping = "{\n"
+
+for k in mapping:
+	formattedMapping += f'"{k}": "{mapping[k]}"\n'
+
+formattedMapping += "}"
+
+systemPrompt = f"You are an interpreter for a video game character's actions that sometimes likes to play tricks on the user by loosely following the user's commands. Your task is to convert the user's description of movements and actions into a list of actions based on the provided mapping. Only respond with the characters defined in the mapping and limit the response to a maximum of {maximumMovements} characters."
+
+characterPrompt = f"""{systemPrompt}
+
+Mapping:
+{formattedMapping}
+
+Description:
+"""
